fa9725add6c983ad0ff0c5ce3314ad1539ae9079
Shir5 <Vova0120_black@icloud.com>
2025-03-07 14:34:28 +0300

    sound method modification

--- src/main/java/task3/Creature.java
+++ src/main/java/task3/Creature.java
@@ -3,6 +3,8 @@ package task3;
 import java.util.ArrayList;
 import java.util.List;
 
+import static task3.Emotion.*;
+
 abstract class Creature {
     protected String name;
     protected Size size;
@@ -10,13 +12,15 @@ abstract class Creature {
     protected int strength;
     protected Voice voice;
     protected List<Action> actions = new ArrayList<>();
+    protected Enum emotion;
 
-    public Creature(String name, Size size, double weight, int strength, Voice voice) {
+    public Creature(String name, Size size, double weight, int strength, Voice voice, Emotion emotion) {
         this.name = name;
         this.size = size;
         this.weight = weight;
         this.strength = strength;
         this.voice = voice;
+        this.emotion = emotion;
     }
 
     public void performAction(ActionType type, String description) {
@@ -27,6 +31,29 @@ abstract class Creature {
 
 
     public double computeSoundVolume() {
-        return voice.getVolume() * Math.log(strength + 1) * size.getFactor();
+
+
+        double baseVolume = voice.getVolume() + 1;
+
+
+        double strengthFactor = Math.sqrt(this.strength + 5);
+
+
+        double sizeAmplifier = Math.exp(this.size.getFactor() / 2.0);
+
+
+        double emotionModifier = 1.0;
+        if (this.emotion.equals(HAPPY)) {
+            emotionModifier = 1.10;
+        } else if (this.emotion.equals(ANGRY)) {
+            emotionModifier = 1.30;
+        } else if (this.emotion.equals(PANICKING)) {
+            emotionModifier = 1.50;
+        } else if (this.emotion.equals(SCARED)) {
+            emotionModifier = 0.80;
+        }
+
+
+        return baseVolume * strengthFactor * sizeAmplifier * emotionModifier;
     }
 }
